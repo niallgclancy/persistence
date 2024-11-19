2cceb8ed3a1b3c9770909791f1d7a1a9507fd993
Niall Clancy <niallgc66@gmail.com>
2024-11-19 14:02:36 -0700

    Testing beta regression error
    Error: Convergence problem. Try using a different family, removing extreme observations, rescaling the response variable (if continuous), fixing ie at a known, non-zero value (via spcov_initial), or fixing dispersion at one (via dispersion_initial).

--- persist/persistSSN.R
+++ persist/persistSSN.R
@@ -1,17 +1,17 @@
 ################################################################
-# R script for SSNbler_vignette.pdf ####
-#
-# Includes additional comments and R code not included in the
-# SSNbler vignete
+# R script for Clancy Dissertation Chapter on Empirical Characteristics of Refugia
+
+
+
 ################################################################
 library(SSNbler)
 library(SSN2)
 library(caret)
 library(tidyverse)
 library(sf)
-# ------------- Import and view the input data ----------------
 
-## Load the sf package and import the streams, observation sites
+
+## import the streams, observation sites
 streams <- st_read("NSI_fix6.shp")
 obs <- st_read("PersistenceMetrics.shp")
 
@@ -21,9 +21,6 @@ streams <- st_transform(streams, crs = 5070)
 obs <- st_transform(obs, crs = 5070)
 
 
-## Look at column names
-names(obs)
-
 ## Plot the data using ggplot2
 ggplot() +
   geom_sf(data = streams) +
@@ -31,12 +28,10 @@ ggplot() +
   coord_sf(datum = st_crs(streams))
 
 # --------------- Build the LSN -------------------------------
-
 ## Set the lsn.path variable
 lsn.path <- "persist_LSN/"
 
-## Build the LSN and take note of the messages printed to the console
-
+## Build the LSN
 edges <- lines_to_lsn(
   streams = streams,
   lsn_path = lsn.path,
@@ -48,7 +43,6 @@ edges <- lines_to_lsn(
 
 
 # ----------- Incorporate sites into LSN -----------------------
-
 ## Incorporate observations. Pay attention to output messages in R
 obs <- sites_to_lsn(
   sites = obs,
@@ -73,7 +67,6 @@ edges <- updist_edges(
 
 
 # ---------- Calculate upstream distance for sites -----------
-
 site.list <- updist_sites(
   sites = list(
     obs = obs),
@@ -92,7 +85,6 @@ ggplot() +
   scale_color_viridis_c()
 
 # ------- Calculate AFV for edges --------------------------------
-
 ## Summarize h2oAreaKm2 and check for zeros
 summary(edges$TotDASqKM) 
 
@@ -106,7 +98,6 @@ edges <- afv_edges(
 
 
 # ------- Calculate AFV for sites -------------------------------
-
 site.list <- afv_sites(
   sites = site.list,    ## Input is a named list
   edges = edges,
@@ -117,7 +108,6 @@ site.list <- afv_sites(
 
 
 # ---------- Assemble the SSN Object -----------------------------
-
 nssn <- ssn_assemble(
   edges = edges,
   lsn_path = lsn.path,
@@ -154,7 +144,6 @@ ggplot() +
 
 
 # ---- Create Distance Matrices ----------------------------------
-
 ## Generate hydrologic distance matrices
 ssn_create_distmat(nssn)
 
@@ -163,63 +152,83 @@ obs.distmat<- ssn_get_stream_distmat(nssn,
                                      name = "obs")
 names(obs.distmat)
 colnames(obs.distmat[[1]])
-#View(obs.distmat[[1]])
 
 ## Create symmetric hydrologic distance matrix
 obs.distmat2 <- obs.distmat[[1]] + t(obs.distmat[[1]])
-#View(obs.distmat2)
+
 
 
 
 
 # ---- Fit spatial stream-network model --------------------------
-
 ## Fit a spatial linear model to Summer mean temperature with a
 ## mixture of TU/TD/EUC covariance models
 library(PerformanceAnalytics)
 
-pairs(S1_93_11 ~ ELEV+SLOPE+CANOPY+F_MAUG_HIS+F_BFI_HIST, data = nssn$obs)
-mdata=obs[,c(7,8,9,10,15,22,23)]
+
+#----------------Identify predictors with multicolinearity 
+mdata=obs[,c(15,22,23, 24,25,127)]
 mdata=as.data.frame(mdata)
 mdata$geometry=NULL
 chart.Correlation(mdata, histogram=TRUE, pch=19)
+#None of the selected predictors (for data exploration) are highly correlated (max r = 0.28)
 
+#--------------------Beta regression can't use 0 or 1...convert to near 0 and 1--------------------
 nssn$obs$persNative=as.numeric(nssn$obs$persNative)
 nssn2=nssn
+
+#Native Sp. Persistence
 nssn2$obs$persNative=as.numeric(nssn2$obs$persNative)
 nssn2$obs$persNative[which(nssn2$obs$persNative==0)]=0.000000001
 nssn2$obs$persNative[which(nssn2$obs$persNative==1)]=0.999999999
 
+#Full Community Persistence
 nssn2$obs$persComm=as.numeric(nssn2$obs$persComm)
 nssn2$obs$persComm[which(nssn2$obs$persComm==0)]=0.000000001
 nssn2$obs$persComm[which(nssn2$obs$persComm==1)]=0.999999999
-nssn2$obs$persComm
+anyNA(nssn2$obs$persComm)
 
-#niob_ssn$obs$Agency[which(is.na(niob_ssn$obs$Agency))]="None"
+
 hist(nssn2$obs$persComm)
-hist(nssn2$obs$Yrange)
 
-nssn2$obs$LKCH=as.numeric(nssn2$obs$LKCH)
-nssn2$obs$LKCH[which(nssn2$obs$LKCH==1)]=NA
-nssn2$obs$LKCH[which(nssn2$obs$LKCH==0)]=1
-nssn2$obs$LKCH[which(nssn2$obs$LKCH==-1)]=0
-nssn2$obs$FHMN=as.numeric(nssn2$obs$FHMN)
-nssn2$obs$FHMN[which(nssn2$obs$FHMN==1)]=NA
-nssn2$obs$FHMN[which(nssn2$obs$FHMN==0)]=1
-nssn2$obs$FHMN[which(nssn2$obs$FHMN==-1)]=0
-nssn2$obs$NativeNet=as.numeric(nssn2$obs$NativeNet)
-nssn2$obs$nColonizSp=as.numeric(nssn2$obs$nColonizSp)
 
-###############################Beta Regression for proportion metrics not working 11-18-2024
+#Individual Species Test
+#nssn2$obs$LKCH=as.numeric(nssn2$obs$LKCH)
+#nssn2$obs$LKCH[which(nssn2$obs$LKCH==1)]=NA
+#nssn2$obs$LKCH[which(nssn2$obs$LKCH==0)]=1
+#nssn2$obs$LKCH[which(nssn2$obs$LKCH==-1)]=0
+
+
+###############################Beta Regression for proportion metrics not working 11-19-2024
+
+##Scale covariates to determine variable importance
 ssn_mod <- ssn_glm(
-  formula =   LKCH~ S1_93_11+F_MAUG_HIS+Yrange+DSbarrier+Length_km+nnPreds,
-  family = "binomial",
+  formula =   persComm~ scale(S1_93_11)+scale(F_MAUG_HIS)+scale(Yrange)+scale(DSbarrier)+scale(Length_km)+scale(nnPreds),
+  family = "beta",
   ssn.object = nssn2,
   tailup_type = "exponential",
   taildown_type = "exponential",
   euclid_type = "exponential",
   additive = "afvArea", estmethod = "ml"
 )
+##Non-scaled version for comparison 
+ssn_mod <- ssn_glm(
+  formula =   persComm~ S1_93_11+F_MAUG_HIS+Yrange+DSbarrier+Length_km+nnPreds,
+  family = "beta",
+  ssn.object = nssn2,
+  tailup_type = "exponential",
+  taildown_type = "exponential",
+  euclid_type = "exponential",
+  additive = "afvArea", estmethod = "ml"
+)
+
+
+
+
+
+
+
+
 
 
 #reg.glm=glm(persComm ~ S1_93_11+F_MAUG_HIS+Yrange+DSbarrier+Length_km+nnPreds, data = nssn2$obs,
