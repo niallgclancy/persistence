a324f3446c395f56fc8e2e9fdd013278c6915839
Niall Clancy <niallgc66@gmail.com>
2024-11-18 20:15:22 -0700

    SSN preparation for persistence metrics

--- persist/persistSSN.R
+++ persist/persistSSN.R
@@ -8,20 +8,12 @@ library(SSNbler)
 library(SSN2)
 library(caret)
 library(tidyverse)
+library(sf)
 # ------------- Import and view the input data ----------------
 
-## Copy the example dataset to a temporary directory
-#copy_streams_to_temp()
-#path <- paste0(tempdir(), "/streamsdata")
-
-## List local input files
-#list.files(path)
-
-## Load the sf package and import the streams, observation sites, and
-## two prediction datasets
-library(sf)
+## Load the sf package and import the streams, observation sites
 streams <- st_read("NSI_fix6.shp")
-obs <- st_read("WILDCARD_SNAPPED.shp")
+obs <- st_read("PersistenceMetrics.shp")
 
 streams=st_cast(streams, to="LINESTRING")
 
@@ -144,7 +136,6 @@ nssn <- ssn_assemble(
 ## added when the SSN was assembled
 class(nssn$obs)
 names(nssn$obs)
-View(nssn$obs)
 
 ## Plot ssn
 ggplot() +
@@ -172,32 +163,11 @@ obs.distmat<- ssn_get_stream_distmat(nssn,
                                      name = "obs")
 names(obs.distmat)
 colnames(obs.distmat[[1]])
-View(obs.distmat[[1]])
+#View(obs.distmat[[1]])
 
 ## Create symmetric hydrologic distance matrix
 obs.distmat2 <- obs.distmat[[1]] + t(obs.distmat[[1]])
-View(obs.distmat2)
-
-
-
-
-
-
-
-
-############Create Persistence dataset [Left here 11-17-2024]
-
-
-
-
-
-
-
-
-
-
-
-
+#View(obs.distmat2)
 
 
 
@@ -206,18 +176,57 @@ View(obs.distmat2)
 
 ## Fit a spatial linear model to Summer mean temperature with a
 ## mixture of TU/TD/EUC covariance models
+library(PerformanceAnalytics)
 
-pairs(cfs ~ bfi+air+pr+gw, data = niob_ssn$obs)
+pairs(S1_93_11 ~ ELEV+SLOPE+CANOPY+F_MAUG_HIS+F_BFI_HIST, data = nssn$obs)
+mdata=obs[,c(7,8,9,10,15,22,23)]
+mdata=as.data.frame(mdata)
+mdata$geometry=NULL
+chart.Correlation(mdata, histogram=TRUE, pch=19)
+
+nssn$obs$persNative=as.numeric(nssn$obs$persNative)
+nssn2=nssn
+nssn2$obs$persNative=as.numeric(nssn2$obs$persNative)
+nssn2$obs$persNative[which(nssn2$obs$persNative==0)]=0.000000001
+nssn2$obs$persNative[which(nssn2$obs$persNative==1)]=0.999999999
+
+nssn2$obs$persComm=as.numeric(nssn2$obs$persComm)
+nssn2$obs$persComm[which(nssn2$obs$persComm==0)]=0.000000001
+nssn2$obs$persComm[which(nssn2$obs$persComm==1)]=0.999999999
+nssn2$obs$persComm
+
 #niob_ssn$obs$Agency[which(is.na(niob_ssn$obs$Agency))]="None"
-ssn_mod <- ssn_lm(
-  formula = sttemp ~ cfs+bfi+air+pr+gw,
-  ssn.object = niob_ssn,
-  tailup_type = "mariah",
-  taildown_type = "mariah",
+hist(nssn2$obs$persComm)
+hist(nssn2$obs$Yrange)
+
+nssn2$obs$LKCH=as.numeric(nssn2$obs$LKCH)
+nssn2$obs$LKCH[which(nssn2$obs$LKCH==1)]=NA
+nssn2$obs$LKCH[which(nssn2$obs$LKCH==0)]=1
+nssn2$obs$LKCH[which(nssn2$obs$LKCH==-1)]=0
+nssn2$obs$FHMN=as.numeric(nssn2$obs$FHMN)
+nssn2$obs$FHMN[which(nssn2$obs$FHMN==1)]=NA
+nssn2$obs$FHMN[which(nssn2$obs$FHMN==0)]=1
+nssn2$obs$FHMN[which(nssn2$obs$FHMN==-1)]=0
+nssn2$obs$NativeNet=as.numeric(nssn2$obs$NativeNet)
+nssn2$obs$nColonizSp=as.numeric(nssn2$obs$nColonizSp)
+
+###############################Beta Regression for proportion metrics not working 11-18-2024
+ssn_mod <- ssn_glm(
+  formula =   LKCH~ S1_93_11+F_MAUG_HIS+Yrange+DSbarrier+Length_km+nnPreds,
+  family = "binomial",
+  ssn.object = nssn2,
+  tailup_type = "exponential",
+  taildown_type = "exponential",
   euclid_type = "exponential",
-  random = ~ as.factor(Year),
   additive = "afvArea", estmethod = "ml"
 )
+
+
+#reg.glm=glm(persComm ~ S1_93_11+F_MAUG_HIS+Yrange+DSbarrier+Length_km+nnPreds, data = nssn2$obs,
+ #   family = "binomial")
+
+#summary(reg.glm)
+
 
 ## Summarise model results
 summary(ssn_mod)
@@ -297,34 +306,3 @@ loocv_mod$RMSPE
 #:::::::::::::::::::::::::::::::::::::
 # Prediction for historical period (1993-2011)####
 #:::::::::::::::::::::::::::::::::::::
-
-## Make predictions at all locations in the pred1km data
-library(tidyverse)
-names(niob_ssn$preds$pred1km)[names(niob_ssn$preds$pred1km) == 'air9311'] <- 'air'
-names(niob_ssn$preds$pred1km)[names(niob_ssn$preds$pred1km) == 'pr9311'] <- 'pr'
-## Recreate model with updates names for air and precip
-ssn_mod <- ssn_lm(
-  formula = sttemp ~ cfs+bfi+air+pr+gw,
-  ssn.object = niob_ssn,
-  tailup_type = "mariah",
-  taildown_type = "mariah",
-  euclid_type = "exponential",
-  #random = ~ as.factor(Year),
-  additive = "afvArea", estmethod = "reml"
-)
-
-predict(ssn_mod, newdata = "pred1km")
-
-## Augment prediction data with predictions
-aug_preds <- augment(ssn_mod, newdata = "pred1km")
-aug_preds[, ".fitted"]
-
-
-## Visualize predictions on the network
-ggplot() +
-  geom_sf(data = aug_preds, aes(color = .fitted), size = 2) +
-  scale_color_viridis_b()
-
-## Write out augmented prediction data to a geopackage
-st_write(aug_preds, paste0("aug_preds_93-11.gpkg"))
-
